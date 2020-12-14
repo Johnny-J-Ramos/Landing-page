52a2719f28384dae42e07c5c04210354214a31fd
Johnny Jefferson <johnnyjeffersonjr@hotmail.com>
2020-12-13 22:54:08 -0300

    concluindo a landing page

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+  document.querySelector('.carv').src = anything;
+  }
+  
+function changeCircleColor(color) {
+  const cicle = document.querySelector('.cicle');
+  cicle.style.background = color;
+}
